99247101ae8ec3faad4a7c23133fdfd8bdef7269
Mixone-FinallyHere <miguel.terol.93@gmail.com>
2021-05-06 16:49:13 +0200

    Added the Civilized Boat Exit

--- TestRunner/TestRunner.cs
+++ TestRunner/TestRunner.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 namespace TestRunner
 {
     [BepInPlugin(ID, title, version)]
-    public class ValheimModTemplate : BaseUnityPlugin
+    public class TestRunner : BaseUnityPlugin
     {
         public const string ID = "mixone.valheim.testrunner";
         public const string version = "0.0.0.0";     
@@ -36,42 +36,6 @@ namespace TestRunner
 
     #region Patches 
 
-    #region Ladder
-
-    [HarmonyPatch(typeof(Ladder), nameof(Ladder.Interact))]
-    public static class Ladder_Interact_Patch
-    {
-        public static Vector3 targetPos;
-        public static Quaternion targetRot;
-        public static Vector3 targetFor;
-
-        public static bool Prefix(ref Ladder __instance, ref bool __result, ref Humanoid character, ref bool hold)
-        {
-            if (hold)
-            {
-                __result = false;
-                return false;
-            }
-            if (!__instance.InUseDistance(character))
-            {
-                __result = false;
-                return false;
-            }
-            targetPos = character.transform.position;
-            targetRot = character.transform.rotation;
-            targetFor = character.GetLookDir();
-            character.transform.position = __instance.m_targetPos.position;
-            character.transform.rotation = __instance.m_targetPos.rotation;
-            character.SetLookDir(__instance.m_targetPos.forward);
-            __instance.m_targetPos.position = targetPos;
-            __instance.m_targetPos.rotation = targetRot;
-            __instance.m_targetPos.forward = targetFor;
-            __result = false;
-            return false;
-        }
-    }
-
-    #endregion
 
     #endregion
 }
